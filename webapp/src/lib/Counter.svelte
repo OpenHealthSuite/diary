bee0e04ee17a8b766799240dfd78c31a2c429d02
Lee Martin <hello@leejohnmartin.co.uk>
2022-08-27 00:21:21 +0100

    Stripping back and getting attaction in

--- webapp/src/lib/Counter.svelte
+++ webapp/src/lib/Counter.svelte
@@ -1,10 +1,11 @@
 <script lang="ts">
+  import { Button } from 'attractions';
   let count: number = 0
   const increment = () => {
     count += 1
   }
 </script>
 
-<button on:click={increment}>
+<Button on:click={increment}>
   count is {count}
-</button>
+</Button>
