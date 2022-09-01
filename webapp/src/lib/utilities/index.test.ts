c01b9cf546b7455bf3367d85462a47fddffda367
Lee Martin <hello@leejohnmartin.co.uk>
2022-09-02 00:17:36 +0100

    Fixing up headers for API Requests

--- webapp/src/lib/utilities/index.test.ts
+++ webapp/src/lib/utilities/index.test.ts
@@ -7,9 +7,20 @@ describe('apiFetch', () => {
     const inputUrls = ["/someroute?this=query", "/route", "/this/is/nested"]
     const testMatrix: any[][] = inputUrls.reduce((prev, curr) => [...prev, ...inits.map(m => [curr, m])], [])
 
-    const alreadyApi = ["/api/somewhere", { url: "/api/somehow"}, "/api", { url: "/api"}]
+    const alreadyApiGet = ["/api/somewhere", "/api"]
+    const alreadyApiObject = [{ url: "/api/somehow"}, { url: "/api" }]
 
-    it.each(alreadyApi)('Given already has /api at start :: does not prepend', async (input) => {
+    it.each(alreadyApiGet)('Given already has /api at start :: does not prepend', async (input) => {
+        const response = { whoami: "fetchResponse" };
+        const fakeFetch = vi.fn().mockResolvedValueOnce(response);
+
+        const result = await apiFetch(input, undefined, fakeFetch);
+
+        expect(fakeFetch).toBeCalledWith(input, undefined)
+        expect(result).toBe(response)
+    })    
+    
+    it.each(alreadyApiObject)('Given already has /api at start object :: does not prepend', async (input) => {
         const response = { whoami: "fetchResponse" };
         const fakeFetch = vi.fn().mockResolvedValueOnce(response);
 
@@ -25,7 +36,7 @@ describe('apiFetch', () => {
 
         const result = await apiFetch(url, init as any, fakeFetch);
 
-        expect(fakeFetch).toBeCalledWith("/api" + url, init)
+        expect(fakeFetch).toBeCalledWith("/api" + url, init ? { ...init, headers: { 'Content-Type': 'application/json' } } : undefined)
         expect(result).toBe(response)
     })
 
@@ -35,7 +46,7 @@ describe('apiFetch', () => {
 
         const result = await apiFetch({ url }, init as any, fakeFetch);
 
-        expect(fakeFetch).toBeCalledWith({ url: '/api' + url }, init)
+        expect(fakeFetch).toBeCalledWith({ url: '/api' + url,  }, init ? { ...init, headers: { 'Content-Type': 'application/json' } } : undefined)
         expect(result).toBe(response)
     })
 
@@ -46,7 +57,45 @@ describe('apiFetch', () => {
         const input = { url, integrity: "someValue" };
         const result = await apiFetch(input, init as any, fakeFetch);
 
-        expect(fakeFetch).toBeCalledWith({ url: '/api' + url, integrity: "someValue" }, init)
+        expect(fakeFetch).toBeCalledWith({ url: '/api' + url, integrity: "someValue" }, init ? { ...init, headers: { 'Content-Type': 'application/json' } } : undefined)
+        expect(result).toBe(response)
+    })
+
+    const methodsWithContent = ["POST", "PUT"]
+
+    it.each(methodsWithContent)('Given %s request object without provided content type header or any headers :: sets application/json', async (method) => {
+        const response = { whoami: "fetchResponse" };
+        const fakeFetch = vi.fn().mockResolvedValueOnce(response);
+
+        const input = { url: "/somewhere" };
+        const result = await apiFetch(input, { method } as any, fakeFetch);
+
+        expect(fakeFetch).toBeCalledWith({ url: '/api' + "/somewhere" }, { method, headers: { 'Content-Type': 'application/json' }})
+        expect(result).toBe(response)
+    })
+
+
+    const inputCTHeaders = ["text/plain", "image/bmp"]
+    it.each(inputCTHeaders)('Given %s request object with provided content type header :: does not overwrite header', async (header) => {
+        const response = { whoami: "fetchResponse" };
+        const fakeFetch = vi.fn().mockResolvedValueOnce(response);
+
+        const input = { url: "/somewhere" };
+        const result = await apiFetch(input as any, { headers: { 'Content-Type': header } } as any, fakeFetch);
+
+        expect(fakeFetch).toBeCalledWith({ url: '/api' + "/somewhere" }, { headers: { 'Content-Type': header }})
+        expect(result).toBe(response)
+    })
+
+    it("Given request with many headers but not content type :: sets content type and preserves rest", async () => {
+        const response = { whoami: "fetchResponse" };
+        const fakeFetch = vi.fn().mockResolvedValueOnce(response);
+
+        const input = { url: "/somewhere" };
+        const init = { headers: { 'someheader': 'aheaders', 'anotherheader' : 'theotherheader' } } ;
+        const result = await apiFetch(input as any, init as any, fakeFetch);
+
+        expect(fakeFetch).toBeCalledWith({ url: '/api' + "/somewhere" }, { headers: { ...init.headers, 'Content-Type': 'application/json' } })
         expect(result).toBe(response)
     })
 })
--- webapp/src/lib/utilities/index.ts
+++ webapp/src/lib/utilities/index.ts
@@ -1,6 +1,19 @@
 export function apiFetch(input: Partial<RequestInfo>, init?: RequestInit, fetchFn = fetch): Promise<Response> {
     if (typeof input === "string") {
-        return fetchFn(input.startsWith('/api') ? input : '/api' + input, init);
+        return fetchFn(input.startsWith('/api') ? input : '/api' + input, init ? { 
+            ...init, 
+            headers: { 
+                ...init.headers, 
+                'Content-Type': init.headers && init.headers['Content-Type'] ? init.headers['Content-Type'] : 'application/json' 
+            }}: undefined);
     }
-    return fetchFn({ ...input, url: input.url.startsWith('/api') ? input.url : '/api' + input.url } as RequestInfo, init);
+    return fetchFn({ 
+        ...input, 
+        url: input.url.startsWith('/api') ? input.url : '/api' + input.url, 
+    } as RequestInfo, init ? { 
+        ...init, 
+        headers: { 
+            ...init.headers, 
+            'Content-Type': init.headers && init.headers['Content-Type'] ? init.headers['Content-Type'] : 'application/json' 
+        }}: undefined);
 }
