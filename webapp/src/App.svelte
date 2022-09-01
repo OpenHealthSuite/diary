33352993c661faeff790edb49171389709dbcef4
Lee Martin <hello@leejohnmartin.co.uk>
2022-09-02 00:18:00 +0100

    Added: Crude list/dialog - dialog seems to work, not list

--- webapp/src/App.svelte
+++ webapp/src/App.svelte
@@ -1,7 +1,24 @@
 <script lang="ts">
+import { Button, Dialog, Modal } from "attractions";
+
+import DailyLog from "./lib/components/DailyLog.svelte";
+import LogEntryInterface from "./lib/components/LogEntryInterface.svelte";
+
+let modalOpen = false;
+
 </script>
 
 <main>
+    <DailyLog day={new Date()} />
+    <Button on:click={() => modalOpen = true}>Add Log</Button>
+    <Modal bind:open={modalOpen} let:closeCallback>
+      <Dialog title="Hello?" {closeCallback}>
+        {#if modalOpen}
+            <LogEntryInterface on:success={closeCallback}/>
+        {/if}
+      </Dialog>
+    </Modal>
+
 </main>
 
 <style>
