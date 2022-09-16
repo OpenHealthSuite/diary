3d6cf2b9156865f6e96d5c0ae3237118c89cb6fa
Lee Martin <hello@leejohnmartin.co.uk>
2022-09-16 22:48:27 +0100

    Styling: Day Selector

--- webapp/src/lib/components/DaySelector.svelte
+++ webapp/src/lib/components/DaySelector.svelte
@@ -10,28 +10,51 @@
     }
 </script>
 <div class="control-row">
-    <button data-testid="backward-button" on:click={() => {
-        day.setDate(day.getDate() - 1)
-        day = day
-        dispatch('dateChange', day)
-    }}>&lt;</button>
-    <fieldset>
-    <label for="day-selected">Day Selected</label>
-    <input type="date" id="day-selected" name="day-selected"
-        pattern="\d{4}-\d{2}-\d{2}"
-        value={day.toISOString().split('T')[0]}
-        on:change={dateUpdater}> 
-    </fieldset>
-    <button data-testid="forward-button" on:click={() => {
-        day.setDate(day.getDate() + 1)
-        day = day
-        dispatch('dateChange', day)
-    }}>&gt;</button>
+    <button data-testid="backward-button"
+        class="control-button" 
+        on:click={() => {
+            day.setDate(day.getDate() - 1)
+            day = day
+            dispatch('dateChange', day)
+        }}>&lt;</button>
+    <div>
+        <label for="day-selected" hidden>Day Selected</label>
+        <input type="date" id="day-selected" 
+            name="day-selected"
+            pattern="\d{4}-\d{2}-\d{2}"
+            class="day-selector"
+            value={day.toISOString().split('T')[0]}
+            on:change={dateUpdater}> 
+    </div>
+    <button data-testid="forward-button"  
+        class="control-button" 
+        on:click={() => {
+            day.setDate(day.getDate() + 1)
+            day = day
+            dispatch('dateChange', day)
+        }}>&gt;</button>
 </div>
 <style lang="scss">
     .control-row {
         display: flex;
         align-items: center;
         justify-content: center;
+    }
+    .control-button {
+        margin: 1em;
+        padding: 1.2em 0.6em;
+        border-radius: 0.5em;
+        border: none;
+        line-height: 0;
+        font-weight: 700;
+        font-size: 1.3em;
+        background-color: rgba(0,0,0,0.2);
+    }
+    .day-selector {
+        font-size: 1.2em;
+        padding: 0.5em;
+        border: 0;
+        border-bottom: 1px solid black;
+        background-color: rgba(0,0,0,0.05);
     }
 </style>
