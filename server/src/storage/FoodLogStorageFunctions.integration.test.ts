3f7355fd8b409e9ff28dd7902a89318ae071a6ad
Lee Martin <hello@leejohnmartin.co.uk>
2022-12-11 00:16:59 +0000

    Refactor: Isolate test configs

--- server/src/storage/FoodLogStorageFunctions.integration.test.ts
+++ server/src/storage/FoodLogStorageFunctions.integration.test.ts
@@ -1,44 +1,7 @@
-import * as sqlite3 from "./sqlite3";
-import * as cassandra from "./cassandra";
 import crypto from "node:crypto";
 import { CreateFoodLogEntry, isNotFoundError } from "./types";
-import knex from "knex";
-import { Client } from "cassandra-driver";
 
-const configs = [
-  {
-    name: "sqlite3",
-    config: {
-      beforeAllSetup: async () => {
-        const testClient = knex({
-          ...sqlite3.DEFAULT_CLIENT_CONFIG,
-          connection: { filename: ":memory:" },
-        });
-        await sqlite3.setupDatabase(testClient);
-        return testClient;
-      },
-      afterAllTeardown: async (testClient: any) => {
-        await testClient.destroy();
-      },
-      storage: sqlite3.sqlite3.foodLog,
-    },
-  },
-  {
-    name: "cassandra",
-    config: {
-      beforeAllSetup: async () => {
-        await cassandra.setupDatabase(
-          new Client(cassandra.DEFAULT_CLIENT_CONFIG)
-        );
-        return new Client(cassandra.DEFAULT_CLIENT_CONFIG);
-      },
-      afterAllTeardown: async (testClient: any) => {
-        await testClient.shutdown();
-      },
-      storage: cassandra.cassandra.foodLog,
-    },
-  },
-];
+import { configs } from "./_testConfigs";
 
 describe.each(configs)(
   "$name Food Log Storage Integration Tests",
