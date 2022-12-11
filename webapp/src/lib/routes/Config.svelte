a685c7e441e3af0c253387866a4982818c8ca454
Lee Martin <hello@leejohnmartin.co.uk>
2022-12-11 00:05:44 +0000

    Update: Scaffolding for config

--- webapp/src/lib/routes/Config.svelte
+++ webapp/src/lib/routes/Config.svelte
@@ -1,7 +1,33 @@
 <script lang="ts">
 
 </script>
-<h1>Config</h1>
+<div class="container">
+  <h1>Config</h1>
+  <div class="config-container">
+    <div class="config-item">
+      Coming soon...
+    </div>
+  </div>
+</div>
 <style lang="scss">
-
+  .container {
+    width: 100vw;
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+  }
+  .config-container {
+    width: 100%;
+    display: flex;
+    flex-direction: row;
+    flex-wrap: wrap;
+    align-items: center;
+    justify-content: space-evenly;
+    .config-item {
+      margin: 1em;
+      padding: 1em;
+      border-radius: 1em;
+      border: 1px solid #000;
+    }
+  }
 </style>
