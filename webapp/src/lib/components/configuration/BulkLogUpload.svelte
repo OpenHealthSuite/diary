805efba43df287f595f7fba3f57761b9de91030d
Lee Martin <hello@leejohnmartin.co.uk>
2022-12-27 20:59:34 +0000

    Fix: Handle parse errors more gracefully

--- webapp/src/lib/components/configuration/BulkLogUpload.svelte
+++ webapp/src/lib/components/configuration/BulkLogUpload.svelte
@@ -9,7 +9,7 @@ let modalOpen = false;
 let files: FileList = undefined;
 
 let preparing = false;
-let uploading = false;
+let parseError = undefined;
 
 let uploaded = 0;
 let errors = 0;
@@ -20,50 +20,56 @@ let newLogs: FoodLogEntry[] = [];
 let editLogs: FoodLogEntry[] = [];
 
 const filesUpdated = async () => {
-  preparing = true;
-  logs = [];
-  newLogs = [];
-  editLogs = [];
-  uploaded = 0;
-  errors = 0;
-  for (let file of files) {
-      const filedata = await file.text();
-      const records = parse(filedata, {
-        columns: true,
-        skip_empty_lines: true,
-      }).map((x: any) => {
-        let log = {
-          ...x,
-          time: {
-            start: x.timeStart,
-            end: x.timeEnd
-          },
-          metrics: JSON.parse(x.metrics),
-          labels: JSON.parse(x.labels),
-        };
-        delete log.timeStart;
-        delete log.timeEnd;
-        return log;
-      }) as FoodLogEntry[];
-      logs = [...logs, ...records];
-  }
-  for (let log of logs) {
-    if (log.id) {
-      const existing = await apiFetch("/logs/" + log.id);
-      if (existing.status === 200) {
-        editLogs.push(log);
-      } else {
-        newLogs.push(log);
-      }
-    } else {
-      newLogs.push(log);
+  try {
+    preparing = true;
+    parseError = undefined;
+    logs = [];
+    newLogs = [];
+    editLogs = [];
+    uploaded = 0;
+    errors = 0;
+    for (let file of files) {
+        const filedata = await file.text();
+        const records = parse(filedata, {
+          columns: true,
+          skip_empty_lines: true,
+        }).map((x: any) => {
+          let log = {
+            ...x,
+            time: {
+              start: x.timeStart,
+              end: x.timeEnd
+            },
+            metrics: x.metrics ? JSON.parse(x.metrics) : {},
+            labels: x.labels ? JSON.parse(x.labels) : [],
+          };
+          delete log.timeStart;
+          delete log.timeEnd;
+          return log;
+        }) as FoodLogEntry[];
+        logs = [...logs, ...records];
     }
+    for (let log of logs) {
+      if (log.id) {
+        const existing = await apiFetch("/logs/" + log.id);
+        if (existing.status === 200) {
+          editLogs = [...editLogs, log];
+        } else {
+          delete log.id;
+          newLogs = [...newLogs, log];
+        }
+      } else {
+        delete log.id;
+        newLogs = [...newLogs, log];
+      }
+    }
+  } catch (ex: any) {
+    parseError = ex.message;
   }
   preparing = false;
 }
 
 const uploadLogs = async () => {
-  uploading = true;
   uploaded = 0;
   errors = 0;
   for (let newLog of newLogs) {
@@ -90,7 +96,6 @@ const uploadLogs = async () => {
             }
         });
   }
-  uploading = false;
 }
 
 </script>
@@ -100,9 +105,13 @@ const uploadLogs = async () => {
 <Modal bind:open={modalOpen}>
   <input type="file" id="input" bind:files={files} on:change={filesUpdated}/>
   {#if preparing}
-    <div>Processing...</div>
+    <div>Processing... (Log {editLogs.length + newLogs.length}/{logs.length})</div>
   {/if}
-  {#if !preparing && !uploading && logs.length > 0}
+  {#if parseError}
+    <div>Error parsing file!</div>
+    <div>{parseError}</div>
+  {/if}
+  {#if !preparing && !parseError && uploaded == 0 && logs.length > 0}
     <ul>
       <li>{logs.length} logs to upload</li>
       <li>{newLogs.length} logs are new (these logs will have new ids)</li>
@@ -110,14 +119,10 @@ const uploadLogs = async () => {
     </ul>
     <button on:click={uploadLogs}>Upload Logs</button>
   {/if}
-  {#if uploading}
-    <ul>
-      <li>Uploaded {uploaded}/{logs.length}</li>
-      <li>Errors: {errors}</li>
-    </ul>
-  {/if}
-  {#if !uploading && (uploaded > 0 || errors > 0)}
+  {#if logs.length > 0 && uploaded + errors === logs.length}
     <h3>Done!</h3>
+  {/if}
+  {#if uploaded > 0}
     <ul>
       <li>Uploaded {uploaded}/{logs.length}</li>
       <li>Errors: {errors}</li>
