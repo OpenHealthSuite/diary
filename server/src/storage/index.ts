fc13dcd958f7b4bfc691fce91b3cb42376d93fa7
Lee <hello@leejohnmartin.co.uk>
2023-01-09 19:59:42 +0000

    Tweak: Remove dependancies and add storage option

--- server/src/storage/index.ts
+++ server/src/storage/index.ts
@@ -3,6 +3,7 @@ export * from "./types/StorageErrors";
 import { cassandra } from "./cassandra";
 import { StorageType } from "./interfaces";
 import { sqlite3 } from "./sqlite3";
+import { storageConfig as neo4j } from "./neo4j"
 
 export interface StorageDrivers {
   [key: string]: StorageType;
@@ -11,6 +12,7 @@ export interface StorageDrivers {
 const AVAILABLE_STORAGES: StorageDrivers = {
   cassandra,
   sqlite3,
+  neo4j
 };
 
 let setupStorage = AVAILABLE_STORAGES.sqlite3;
@@ -18,6 +20,9 @@ let setupStorage = AVAILABLE_STORAGES.sqlite3;
 switch (process.env.OPENFOODDIARY_STORAGE_PROVIDER) {
   case "cassandra":
     setupStorage = AVAILABLE_STORAGES.cassandra;
+    break;
+  case "neo4j":
+    setupStorage = AVAILABLE_STORAGES.neo4j;
     break;
 }
 
