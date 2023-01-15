352df0435c34e92efda6c0ecbdeb33a8f0066a7f
Lee Martin <hello@leejohnmartin.co.uk>
2023-01-15 15:41:14 +0000

    Added: automated e2e test of the container image

--- tests/api/e2e/config.ts
+++ tests/api/e2e/config.ts
@@ -1,4 +1,5 @@
 export const TEST_CONFIGURATION = {
-    API_HOST: process.env.OPENFOODDIARY_API_HOST ?? 'http://localhost:3012',
-    USERID_HEADER: process.env.OPENFOODDIARY_USERID_HEADER ?? 'x-openfooddiary-userid'
-}
+  API_HOST: process.env.OPENFOODDIARY_API_HOST ?? "http://localhost:3012",
+  USERID_HEADER:
+    process.env.OPENFOODDIARY_USERIDHEADER ?? "x-openfooddiary-userid",
+};
