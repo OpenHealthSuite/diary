fc058604f8f658126a313320d3b1794c88517173
Lee Martin <hello@leejohnmartin.co.uk>
2023-03-27 21:42:32 +0100

    Refactor: all storage tests to integration tests

--- server/jest.config.js
+++ server/jest.config.js
@@ -3,5 +3,9 @@ module.exports = {
   preset: 'ts-jest',
   testEnvironment: 'node',
   testMatch: ['**/*.test.ts'],
-  testPathIgnorePatterns: ['/node_modules/', '.+\.integration\.test\.ts$']
+  testPathIgnorePatterns: ['/node_modules/', '.+\.integration\.test\.ts$'],
+  coveragePathIgnorePatterns: [
+    "/node_modules/",
+    "/src/storage/"
+  ]
 };
--- server/src/storage/FoodLogStorageFunctions.integration.test.ts
+++ server/src/storage/FoodLogStorageFunctions.integration.test.ts
@@ -1,12 +1,15 @@
 import crypto from "node:crypto";
 import {
   CreateFoodLogEntry,
+  EditFoodLogEntry,
   StoreFoodLogFunction,
   isNotFoundError,
+  isValidationError,
 } from "./types";
 import fs from "node:fs";
 import { parse } from "csv-parse/sync";
 import { configs } from "./_testConfigs";
+import { METRIC_MAX } from "../config";
 
 describe.each(configs)(
   "$name Food Log Storage Integration Tests",
@@ -384,6 +387,138 @@ describe.each(configs)(
       );
 
       expect(secondReRetreives.every((x) => x === true)).toBeTruthy();
+    });
+  }
+);
+
+
+describe.each(configs)(
+  "$name FoodLogStorageFunctions",
+  ({ config : { storage: { foodLog: config }}} ) => {
+    describe("CreateFoodLog", () => {
+      describe("Validation Errors", () => {
+        const GoldInput: CreateFoodLogEntry = {
+          name: "My Food Log",
+          labels: ["Some Label", "Some other label"],
+          time: {
+            start: new Date(1999, 10, 10),
+            end: new Date(1999, 10, 11),
+          },
+          metrics: {
+            calories: 500,
+          },
+        };
+
+        const testUserId = crypto.randomUUID();
+
+        const { name, ...nameless } = structuredClone(GoldInput);
+        const { labels, ...labelless } = structuredClone(GoldInput);
+        const { metrics, ...metricless } = structuredClone(GoldInput);
+        const nullMetrics = { ...structuredClone(GoldInput), metrics: null };
+        let weirdMetric: any = structuredClone(GoldInput);
+        weirdMetric.metrics.calories = "This is not a number";
+        let oversizeMetric: any = structuredClone(GoldInput);
+        oversizeMetric.metrics.calories = METRIC_MAX + 1;
+        const { time, ...timeless } = structuredClone(GoldInput);
+        let startTimeLess: any = structuredClone(GoldInput);
+        delete startTimeLess.time.start;
+        let endTimeLess: any = structuredClone(GoldInput);
+        delete endTimeLess.time.end;
+        let endBeforeStart = structuredClone(GoldInput);
+        endBeforeStart.time.start = new Date(1999, 10, 10);
+        endBeforeStart.time.end = new Date(1999, 8, 10);
+
+        const BadValues: any[] = [
+          ["Empty", {}],
+          ["WithId", { ...GoldInput, id: crypto.randomUUID() }],
+          ["No Name", nameless],
+          ["No Labels", labelless],
+          ["No Metrics", metricless],
+          ["Null Metrics", nullMetrics],
+          ["Non-number Metric", weirdMetric],
+          ["Metric greater than maxint", oversizeMetric],
+          ["No Times", timeless],
+          ["No Start Time", startTimeLess],
+          ["No End Time", endTimeLess],
+          ["End before start", endBeforeStart],
+        ];
+
+        it.each(BadValues)(
+          "Rejects Bad Test Case %s",
+          async (name: string, badValue: any) => {
+            const result = await config.storeFoodLog(testUserId, badValue);
+
+            expect(result.isErr()).toBeTruthy();
+            expect(isValidationError(result._unsafeUnwrapErr())).toBeTruthy();
+          }
+        );
+      });
+    });
+
+    describe("QueryFoodLog", () => {
+      describe("Validation Errors", () => {
+        it("Rejects disordered dates", async () => {
+          const result = await config.queryFoodLogs(
+            crypto.randomUUID(),
+            new Date(1997, 10, 1),
+            new Date(1987, 10, 1)
+          );
+
+          expect(result.isErr()).toBeTruthy();
+          expect(isValidationError(result._unsafeUnwrapErr())).toBeTruthy();
+        });
+      });
+    });
+
+    describe("EditFoodLog", () => {
+      describe("Validation Errors", () => {
+        const GoldInput: EditFoodLogEntry = {
+          id: crypto.randomUUID(),
+          name: "My Food Log",
+          labels: ["Some Label", "Some other label"],
+          time: {
+            start: new Date(),
+            end: new Date(),
+          },
+          metrics: {
+            calories: 500,
+          },
+        };
+
+        const testUserId = crypto.randomUUID();
+
+        let weirdMetric: any = structuredClone(GoldInput);
+        weirdMetric.metrics.calories = "This is not a number";
+        let oversizeMetric: any = structuredClone(GoldInput);
+        oversizeMetric.metrics.calories = METRIC_MAX + 1;
+        let startTimeLess: any = structuredClone(GoldInput);
+        delete startTimeLess.time.start;
+        let endTimeLess: any = structuredClone(GoldInput);
+        delete endTimeLess.time.end;
+        let endBeforeStart = structuredClone(GoldInput);
+        endBeforeStart.time!.start = new Date(1999, 10, 10);
+        endBeforeStart.time!.end = new Date(1999, 8, 10);
+
+        const BadValues: any[] = [
+          ["Empty", {}],
+          ["WithoutId", { ...GoldInput, id: undefined }],
+          ["Non-number Metric", weirdMetric],
+          ["Metric greater than maxint", oversizeMetric],
+          ["No Start Time", startTimeLess],
+          ["No End Time", endTimeLess],
+          ["End before start", endBeforeStart],
+        ];
+
+        it.each(BadValues)(
+          "Rejects Bad Test Case %s",
+          async (name: string, badValue: any) => {
+            const result = await config.editFoodLog(testUserId, badValue);
+
+            expect(result.isErr()).toBeTruthy();
+            expect(isValidationError(result._unsafeUnwrapErr())).toBeTruthy();
+          }
+        );
+      });
     });
   }
 );
