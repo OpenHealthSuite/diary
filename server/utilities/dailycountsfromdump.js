c9c639dd924afe95925927b2085dc36fb185fd69
Lee <hello@leejohnmartin.co.uk>
2024-11-06 20:00:44 +0000

    chore: need to include "empty" days [skip ci]

--- server/utilities/dailycountsfromdump.js
+++ server/utilities/dailycountsfromdump.js
@@ -45,4 +45,34 @@ const exportedLogs = Object.entries(aggregated).map(([day, metrics]) => [
   ...knownmetrics.map(km => metrics[km] ?? 0)
 ]).sort(([a], [b]) => a.localeCompare(b));
 
-fs.appendFileSync(filename, stringify(exportedLogs));
+// first date
+const firstDate = exportedLogs[0][0];
+// last date
+const lastDate = exportedLogs[exportedLogs.length - 1][0];
+
+function getDatesBetween (startDateStr, endDateStr) {
+  // Parse the date strings into Date objects
+  const startDate = new Date(startDateStr);
+  const endDate = new Date(endDateStr);
+
+  // Array to store each date
+  const dates = [];
+
+  // Iterate over each day
+  // eslint-disable-next-line no-unmodified-loop-condition
+  for (let date = new Date(startDate); date <= endDate; date.setDate(date.getDate() + 1)) {
+    // Add the current date to the array
+    dates.push(new Date(date).toISOString().split("T")[0]); // Format as "YYYY-MM-DD"
+  }
+
+  return dates;
+}
+
+const allDates = getDatesBetween(firstDate, lastDate);
+
+const finalLogs = allDates.map((date) => [
+  date,
+  ...knownmetrics.map(km => aggregated[date] ? aggregated[date][km] ?? 0 : 0)
+]).sort(([a], [b]) => a.localeCompare(b));
+
+fs.appendFileSync(filename, stringify(finalLogs));
