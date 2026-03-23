3037b3f75ef7e5324f2798a29f927e0df47100c5
Lee Martin <hello@leejohnmartin.co.uk>
2026-03-23 21:13:04 +0000

    fix: Goodbye node tests

--- tests/api/e2e/config.ts
+++
@@ -1,5 +0,0 @@
-export const TEST_CONFIGURATION = {
-  API_HOST: process.env.OPENFOODDIARY_API_HOST ?? "http://localhost:3022",
-  USERID_HEADER:
-    process.env.OPENFOODDIARY_USERIDHEADER ?? "x-openfooddiary-userid",
-};
